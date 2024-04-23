33f2539b317847a1a22522bba7c83b2004e0a92b
lenny <lenny.dennis@zeraki.co.ke>
2024-04-23 13:12:10 +0300

    Handle input validation

--- accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -1,14 +1,20 @@
 package com.eazybytes.accounts.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
 
+    @NotEmpty(message = "Name cannot be a null or empty")
+    @Size(min =  5 , max =  30, message = "The legth of the customer name shoule be between 5 and 30")
     private String name;
 
+    @NotEmpty(message = "Email cannot be a null or empty")
+    @Email(message = "Email should be a valid value")
     private String email;
 
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNumber;
 
     private AccountsDto accountsDto;
